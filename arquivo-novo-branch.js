28239751df0ad00ae5f23290a124169085ed304f
elvishuges <elvishuges@hotmail.com>
2021-04-11 16:15:49 -0300

    🎨 mudando arquivo novo branch dev 1

--- arquivo-novo-branch.js
+++ arquivo-novo-branch.js
@@ -1,2 +1,2 @@
 console.log("Correções feitas pelo desenvolvedor 2 (novo branch)");
-console.log("Correções feitas pelo desenvolvedor 1 ");
+console.log("Correções mudando arquivo novo branch dev 2");
